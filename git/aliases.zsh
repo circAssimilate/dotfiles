925d0c75f3dceaa7fb2ba3734691563d0e20c824
Asa Schachar <asa@optimizely.com>
2015-10-28 23:43:03 -0700

    Change git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -6,26 +6,26 @@ then
   alias git=$hub_path
 fi
 
-# The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glo='gl && git pull origin'
-alias glr='git pull --rebase'
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset %Cblue%an%Creset: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push -u origin HEAD'
-alias glrp='glr && gp'
-alias gph='git push heroku HEAD'
+# Aliases
+alias gs='git status'
+alias gso='git show'
+alias gb='git branch'
 alias gd='git diff'
 alias gdc='git diff --cached'
-alias gc='git commit --verbose'
-alias gca='git commit --all --verbose'
-alias gcm='git commit --amend'
+alias gc='git commit'
 alias gco='git checkout'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
-           perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
-alias gdt='git dt'
-alias gstash='git stash -u'
-alias gw="git show"
+alias ga='git add'
+alias gl='git log'
+alias g='git'
+alias gr='git rebase'
+alias gp='git pull'
+alias gm='git merge'
+alias gf='git fetch'
+alias gps='git push'
+alias gcp='git cherry-pick'
+alias gbl='for k in `git branch|perl -pe s/^..//`;do echo -e `git show --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" $k|head -n 1`\\t$k;done|sort -r | head -n 10'
+alias gbl1='gbl|perl -pe s/(?:.+)\\t// | grep -v "^devel$" | grep -v "`git rev-parse --abbrev-ref HEAD`" | head -n 1'
+alias gcol='git checkout `gbl1`'
+alias gbclean='git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d'
 alias gss="git stash"
 alias gsp="git stash pop"
