536137ffcc0ba0dbe9a157ee780258b593f39647
Asa Schachar <asa@optimizely.com>
2015-12-03 19:51:29 -0800

    Fix autocomplete for git branches

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -35,7 +35,7 @@ setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+unsetopt complete_aliases
 
 zle -N newtab
 
