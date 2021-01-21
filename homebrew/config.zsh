fe2a82297ab8261e54da18c50a37c4d5304a4cb0
circassimilate <dehammond@me.com>
2021-01-20 21:28:23 -0600

    remove unused tools

---
+++ homebrew/config.zsh
@@ -0,0 +1,4 @@
+# initialize
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> /Users/derek/.zprofile
+eval $(/opt/homebrew/bin/brew shellenv)
+
--- rbenv/path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH="$PATH:$HOME/.rbenv/bin"
-export PATH="$PATH:$HOME/.gem/ruby/2.3.1/bin" # gem user-install location
